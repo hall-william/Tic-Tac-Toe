0c6ec94853d5ba64ca797f65dababf418e311bc8
William Hall <willhall3@icloud.com>
2017-04-17 11:49:27 -0400

    "Winner" added.

--- Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
@@ -13,7 +13,14 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     var activePlayer = 1 //X goes first
-    var playerImage: UIImage = #imageLiteral(resourceName: "X")
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] //array to hold each space on the board
+    var activeGame = true
+    
+    //all of the winning combinations
+    let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    
+    
+    @IBOutlet weak var winningLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +30,59 @@ class GameViewController: UIViewController {
     
     @IBAction func action(_ sender: UIButton) {
      
-        if(activePlayer == 1)
+        //if the space is empty
+        if(gameState[sender.tag-1] == 0 && activeGame)
         {
-            playerImage = #imageLiteral(resourceName: "X")
-            sender.setImage(playerImage, for: UIControlState())
-            activePlayer = 2
+            //update the sapces
+            gameState[sender.tag-1] = activePlayer
+            
+            if(activePlayer == 1)//player1
+            {
+                sender.setImage(#imageLiteral(resourceName: "X"), for: UIControlState())
+                activePlayer = 2
+            }
+            else//player2
+            {
+                sender.setImage(#imageLiteral(resourceName: "O"), for: UIControlState())
+                activePlayer = 1
+            }
         }
-        else
+        
+        for combination in winningCombinations
         {
-            playerImage = #imageLiteral(resourceName: "O")
-            sender.setImage(playerImage, for: UIControlState())
-            activePlayer = 1
+            if(gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]])
+            {
+                activeGame = false
+                if (gameState[combination[0]] == 1) {
+                    //X won
+                    winningLabel.text = "Player 1 Wins!!!"
+                }
+                else
+                {
+                    //O Won
+                    winningLabel.text = "Player 2 Wins!!!"
+                }
+                
+                winningLabel.isHidden = false
+                resetButton.isHidden = false
+            }
+        }
+    }
+    
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBAction func reset(_ sender: UIButton) {
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        activeGame = true
+        activePlayer = 1
+        winningLabel.isHidden = true
+        resetButton.isHidden = true
+        
+        for i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControlState())
         }
     }
     
