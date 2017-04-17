f0430b8c1fe1694a29234013e90d706c401b1a2b
William Hall <willhall3@icloud.com>
2017-04-17 11:08:05 -0400

    Implemented activePlayer.

--- Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
@@ -12,36 +12,31 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var activePlayer = 1 //X goes first
+    var playerImage: UIImage = #imageLiteral(resourceName: "X")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+       
     }
 
+    
+    @IBAction func action(_ sender: UIButton) {
+     
+        if(activePlayer == 1)
+        {
+            playerImage = #imageLiteral(resourceName: "X")
+            sender.setImage(playerImage, for: UIControlState())
+            activePlayer = 2
+        }
+        else
+        {
+            playerImage = #imageLiteral(resourceName: "O")
+            sender.setImage(playerImage, for: UIControlState())
+            activePlayer = 1
+        }
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
