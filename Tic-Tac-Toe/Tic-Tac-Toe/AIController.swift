bdeb99cf57b9eb541d5391a5bb5626a844c06451
William Hall <willhall3@icloud.com>
2017-04-22 13:10:31 -0400

    Randomly generating AI.

--- Tic-Tac-Toe/Tic-Tac-Toe/AIController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/AIController.swift
@@ -30,39 +30,40 @@ class AIController: UIViewController{
     
     @IBAction func action(_ sender: UIButton) {
         
-        //if the space is empty
+        //if the space is empty and it is the user's turn
         if(gameState[sender.tag-1] == 0 && activeGame && activePlayer == 1)
         {
             //update the sapces
             gameState[sender.tag-1] = activePlayer
             
+            //make sure it is the user's turn
             if(activePlayer == 1)//player1
             {
+                //change image to X
                 sender.setImage(#imageLiteral(resourceName: "X"), for: UIControlState())
                 activePlayer = 2
             }
-            else//player2
+            
+            print("\nPlayer 1 Turn")//delete this later
+            for i in 0 ..< 9
             {
-                sender.setImage(#imageLiteral(resourceName: "O"), for: UIControlState())
-                activePlayer = 1
+                print(i,": " , gameState[i])//delete this later
             }
         }
         
+        //pasre array for winning combination
         for combination in winningCombinations
         {
-            if(gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]])
+            //check for 3 in a row
+            if(gameState[combination[0]] == 1 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]])
             {
                 activeGame = false
                 if (gameState[combination[0]] == 1) {
                     //X won
-                    winningLabel.text = "Player 1 Wins!!!"
-                }
-                else
-                {
-                    //O Won
-                    winningLabel.text = "Player 2 Wins!!!"
+                    winningLabel.text = "You Win!!!"
                 }
                 
+                //allow for reset
                 winningLabel.isHidden = false
                 resetButton.isHidden = false
             }
@@ -71,13 +72,15 @@ class AIController: UIViewController{
             
             for i in gameState
             {
+                //check to see if any blank squares
                 if i == 0
                 {
-                    activeGame = true
+                    activeGame = true//keep playing
                     break
                 }
             }
             
+            //if no blank squares
             if(activeGame == false)
             {
                 winningLabel.text = "It's a Draw!!!"
@@ -85,11 +88,91 @@ class AIController: UIViewController{
                 resetButton.isHidden = false
             }
         }
+        
+        //Computer's Turn
+        AITurn()
+    }
+    
+    ///A function the is responsible for handling the AI's turn.
+    func AITurn()
+    {
+        //Code for AI
+        if(activeGame && activePlayer == 2)
+        {
+            //choose a random space that hasn't alread been selected
+            var selection = Int(arc4random_uniform(9))+1
+            while gameState[selection-1] != 0{
+                selection = Int(arc4random_uniform(9))+1
+            }
+            
+            //change gameState to reflect AI's choice
+            gameState[selection-1] = 2
+            
+            //change square on gameboard to O
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+                // Put your code which should be executed with a delay here
+                let tmpButton = self.view.viewWithTag(selection) as? UIButton
+                tmpButton?.setImage(#imageLiteral(resourceName: "O"), for: UIControlState())
+            })
+            
+            //pasre array for winning combination
+            for combination in winningCombinations
+            {
+                //check for 3 in a row
+                if(gameState[combination[0]] == 2 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]])
+                {
+                    activeGame = false
+                    if (gameState[combination[0]] == 2) {
+                        //O won
+                        winningLabel.text = "You Lose :("
+                    }
+                    
+                    //allow for reset
+                    winningLabel.isHidden = false
+                    resetButton.isHidden = false
+                }
+                
+                activeGame = false
+                
+                for i in gameState
+                {
+                    //check to see if any blank squares
+                    if i == 0
+                    {
+                        activeGame = true//keep playing
+                        break
+                    }
+                }
+                
+                //if no blank squares
+                if(activeGame == false)
+                {
+                    winningLabel.text = "It's a Draw!!!"
+                    winningLabel.isHidden = false
+                    resetButton.isHidden = false
+                }
+            }
+            
+            print("Player 2 Turn")//delete this later
+            print(selection)
+            for i in 0 ..< 9
+            {
+                print(i,": " ,gameState[i])//delete this later
+            }
+        }
+        
+        //change back to user's turn
+        activePlayer = 1
     }
     
     
+    /// An outlet for the reset button in the UI.
     @IBOutlet weak var resetButton: UIButton!
     
+    
+    /// This method handles resetting the game.
+    ///
+    /// - Parameter sender: The reset button in the UI.
     @IBAction func reset(_ sender: UIButton) {
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         activeGame = true
--- Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
@@ -10,25 +10,36 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+
+/// This class is responsible for the GameVsFriend UIViewController
 class GameViewController: UIViewController {
 
+    ///The active player
     var activePlayer = 1 //X goes first
+    
+    ///An array to hold the squares on the gameboard
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] //array to hold each space on the board
+    
+    ///A boolean to hold if the game is currently ongoing
     var activeGame = true
     
     //all of the winning combinations
     let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     
-    
+    ///An outlet for the label that displays the winner of the game.
     @IBOutlet weak var winningLabel: UILabel!
     
-    
+    ///Called after the view has loaded
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
 
-    
+    /**
+      This method is responsible for recieving all of the input from the buttons on the gameboard.
+       - parameters:
+           - sender: the button that was pressed.
+     */
     @IBAction func action(_ sender: UIButton) {
      
         //if the space is empty
@@ -49,6 +60,7 @@ class GameViewController: UIViewController {
             }
         }
         
+        //parse through the winning combinations
         for combination in winningCombinations
         {
             if(gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]])
@@ -70,8 +82,10 @@ class GameViewController: UIViewController {
             
             activeGame = false
             
+            //check each board space
             for i in gameState
             {
+                //if there are still empty spaces
                 if i == 0
                 {
                     activeGame = true
@@ -88,9 +102,12 @@ class GameViewController: UIViewController {
         }
     }
     
-    
+    ///An outlet for the reset button in the UI
     @IBOutlet weak var resetButton: UIButton!
     
+    /// This method handles resetting the game.
+    ///
+    /// - Parameter sender: The reset button in the UI.
     @IBAction func reset(_ sender: UIButton) {
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         activeGame = true
